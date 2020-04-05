7780b9d416e6c69168b601373c32ac89ced07fab
wahyu <wahyu.ehs@gmail.com.com>
2020-04-05 17:57:54 +0700

    initial commit

---
+++ src/main/java/MockArrayList.java
@@ -0,0 +1,69 @@
+
+public class MockArrayList<E> {
+	transient Object[] elementData;
+	private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+	private int index;
+	private int incrementCapacity = 2;
+
+	public MockArrayList() {
+		this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+	}
+
+	public void add(E item) {
+		ensureExplicitCapacity();
+		elementData[index++] = item;
+		System.out.println(index + " and " + elementData.length);
+	}
+
+	public void ensureExplicitCapacity() {
+		if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+			Object[] tempData = new Object[incrementCapacity];
+			elementData = tempData;
+		} else if (elementData.length <= index) {
+			Object[] tempData = new Object[index + incrementCapacity];
+			for (int i = 0; i < elementData.length; i++) {
+				tempData[i] = elementData[i];
+			}
+			elementData = tempData;
+		}
+	}
+
+	public void remove(E item) {
+		for (int i = 0; i < elementData.length; i++) {
+			if (elementData[i] == item) {
+				// shifting elements
+				for (int j = i; j < elementData.length - 1; j++) {
+					elementData[j] = elementData[j + 1];
+				}
+
+				break;
+			}
+		}
+	}
+	
+	public E getItem(int idx) {
+		E val=null;
+		if(idx >= this.index)
+			throw new IndexOutOfBoundsException("array index of bound");
+		for(int i=0; i<this.index; i++) {
+			if(idx == i)
+				val = (E) this.elementData[idx];
+		}
+		return val;
+	}
+
+	public boolean contain(E item) {
+		boolean bol = Boolean.FALSE;
+		for (Object str : elementData) {
+			if (item.equals(str)) {
+				return Boolean.TRUE;
+			}
+		}
+		return bol;
+	}
+	
+	public int getSize() {
+		return this.index;
+	}
+
+}
---
+++ src/test/java/MockArrayListTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class MockArrayListTest {
+	
+	public MockArrayList<String> init() {
+		MockArrayList<String> ls = new MockArrayList<String>();
+		ls.add("1");
+		ls.add("2");
+		ls.add("3");
+		return ls;
+	}
+	
+	@Test
+	public void add() {
+		MockArrayList<String> ls = this.init();
+		int res = ls.getSize();
+		assertEquals("ok add ",3, res);
+	}
+		
+	@Test
+	public void getitemByIndex() {
+		MockArrayList<String> ls =  this.init();
+		String res = ls.getItem(0);
+		assertEquals("1", res);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void getitemByIndexError() {
+		MockArrayList<String> ls =  this.init();
+		ls.getItem(1000);
+	}
+	
+	@Test
+	public void contain() {
+		MockArrayList<String> ls = this.init();
+		boolean bol = ls.contain("1");
+		assertTrue(bol);
+	}
+	
+	@Test
+	public void containError() {
+		MockArrayList<String> ls = this.init();
+		boolean bol = ls.contain("Tari");
+		assertFalse(bol);
+	}
+	
+	@Test
+	public void remove() {
+		MockArrayList<String> ls = this.init();
+		ls.remove("1");
+		assertEquals("Removing 1 list", 2, ls.getSize());
+	}
+	
+	@Test
+	public void remove_1() {
+		MockArrayList<String> ls = this.init();
+		ls.remove("1000");
+		assertEquals("Removing 1 list",4, ls.getSize());
+	}
+	
+	@Test
+	public void getSize() {
+		MockArrayList<String> ls = this.init();
+        assertEquals("getSize", 3, ls.getSize());
+	}
+}
